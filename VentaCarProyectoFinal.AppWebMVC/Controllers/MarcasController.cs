002841820baab2fb7b7d832a9fc3652217fc5654
Ivan Rosa Sandoval <User@WIN-JBH2C2BUVPP>
2025-04-01 21:50:01 -0600

    Crud de Marcas Terminado

---
+++ VentaCarProyectoFinal.AppWebMVC/Controllers/MarcasController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VentaCarProyectoFinal.AppWebMVC.Models;
+
+namespace VentaCarProyectoFinal.AppWebMVC.Controllers
+{
+    public class MarcasController : Controller
+    {
+        private readonly VentacarProyectContext _context;
+
+        public MarcasController(VentacarProyectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Marcas
+        public async Task<IActionResult> Index(Marca marca, int topRegistro = 10)
+        {
+            var query = _context.Marcas.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(marca.Marca1))
+                query = query.Where(s => s.Marca1.Contains(marca.Marca1));
+
+            if (topRegistro > 0)
+                query = query.Take(topRegistro);
+
+            return View(await query.ToListAsync());
+
+
+
+
+        }
+
+        // GET: Marcas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var marca = await _context.Marcas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (marca == null)
+            {
+                return NotFound();
+            }
+
+            return View(marca);
+        }
+
+        // GET: Marcas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Marcas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Marca1")] Marca marca)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(marca);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(marca);
+        }
+
+        // GET: Marcas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var marca = await _context.Marcas.FindAsync(id);
+            if (marca == null)
+            {
+                return NotFound();
+            }
+            return View(marca);
+        }
+
+        // POST: Marcas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Marca1")] Marca marca)
+        {
+            if (id != marca.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(marca);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MarcaExists(marca.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(marca);
+        }
+
+        // GET: Marcas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var marca = await _context.Marcas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (marca == null)
+            {
+                return NotFound();
+            }
+
+            return View(marca);
+        }
+
+        // POST: Marcas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var marca = await _context.Marcas.FindAsync(id);
+            if (marca != null)
+            {
+                _context.Marcas.Remove(marca);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MarcaExists(int id)
+        {
+            return _context.Marcas.Any(e => e.Id == id);
+        }
+    }
+}
--- VentaCarProyectoFinal.AppWebMVC/Models/Marca.cs
+++ VentaCarProyectoFinal.AppWebMVC/Models/Marca.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace VentaCarProyectoFinal.AppWebMVC.Models;
 
@@ -7,6 +8,8 @@ public partial class Marca
 {
     public int Id { get; set; }
 
+    [Display(Name = "Nombre Marca")]
+    [Required(ErrorMessage = "La Marca es obligatorio.")]
     public string Marca1 { get; set; } = null!;
 
     public virtual ICollection<Auto> Autos { get; set; } = new List<Auto>();
